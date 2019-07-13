cf59bfef68d04d2d171d5904e31d51716abcf8f2
Pablo Enfedaque <penfedaque@lbl.gov>
2019-07-12 20:26:50 -0700

    Adding some simulation functions. Not tested yet.

--- gridrec.py
+++ gridrec.py
@@ -12,6 +12,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import h5py
 import dxchange
+import tomopy
+from scipy import misc
 
 #Read in file
 #filename  = '20190524_085542_clay_testZMQ.h5'
@@ -26,9 +28,11 @@ stack_sino = np.random.rand(128,128,128)
 #num_rays   = int(gdata['nrays'])
 #Nr = len(r)
 
-num_slices = 128
-num_angles = 128
-num_rays   = 128
+size = 64
+
+num_slices = size
+num_angles = size
+num_rays   = size
 
 print("The total number of slices is", num_slices, "and the total number of angles is", num_angles)
 
@@ -57,6 +61,7 @@ print("------ Ran FFT ------")
 ## Need to dig into fft shift to figure out how to set the center for the shift.
 qt = np.fft.fftshift(qt, axes=2)
 print("------ Shifted along second axis.--------")
+import sys
 np.set_printoptions(threshold=sys.maxsize)
 
 
@@ -73,36 +78,84 @@ np.set_printoptions(threshold=sys.maxsize)
 # Convolution for one slice
 k_r = 2
 
+def create_unique_folder(name):
+   
+    while True:
+        id_index += 1
+        folder = "Sim_" + dataset_name + "_id_" + str(id_index) + "/"
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+            break
+
+    return folder
+
+def save_cube(cube, base_fname):
+
+    for n in range(0, cube.shape[0]):
+        tname = base_fname + "_" + str(n) + '.tiff'
+        misc.imsave(tname, cube[n,:,:])
+
+
+def generate_Shepp_Logan(cube_shape):
+
+   return tomopy.misc.phantom.shepp3d(size=cube_shape, dtype='float32')
+
+
+def forward_project(cube, theta):
+
+    return tomopy.sim.project.project(np.real(cube), theta, pad=False, sinogram_order=False)
+
 
 @jit 
 #def grid_rec_one_slice(qxy, qt):
-def grid_rec_one_slice(num_rays,qt):
+def grid_rec_one_slice(qt, num_rays):
 
     qxy = np.zeros((num_rays, num_rays))
-#    num_rays, _ = qxy.shape
+
     for q in range(num_rays):
         for theta in range(num_angles):
             px = (q - num_rays/2)*np.cos(theta)+(num_rays/2)
             py = (q - num_rays/2)*np.sin(theta)+(num_rays/2)
             for ii in range(-k_r, k_r):
                 for jj in range(-k_r, k_r):
-                    ## qt[q,theta,0] to get just the first slice.
-                    gaussian_kernel = np.exp(-0.5*(px-round(px)-ii)**2/(2**2)-0.5*(py-round(py)-jj)**2/(2**2))
-                    #print(gaussian_kernel)
-                    #print(int(round(px)+ii-2), int(round(py)+jj-2))
-                    #print(qt[0,theta,q])
-                    qxy[int(round(px)+ii-2), int(round(py)+jj-2)] += qt[0,theta,q]*gaussian_kernel
-                    #print(qxy[int(round(px)+ii-2), int(round(py)+jj-2)])
-#                    print(int(round(px)+ii-2), int(round(py)+jj-2))
-    return qxy                    
-#print("----- Finished convolution for one slice -----")
-#print(qxy)
-#print(qt)
-#qxy = grid_rec_one_slice(qxy,qt)
-qxy = grid_rec_one_slice(num_rays,qt)
 
-print(qxy)
-print("----- Finished one pass of grid rec ------")
+                    gaussian_kernel = np.exp(-0.5*(px-round(px)-ii)**2/(2**2)-0.5*(py-round(py)-jj)**2/(2**2))
+
+                    qxy[int(round(px)+ii-2), int(round(py)+jj-2)] += qt[theta,q]*gaussian_kernel
+
+    return qxy                    
+
+
+base_folder = create_unique_folder("shepp_logan")
+
+
+true_obj_shape = (num_slices, num_angles, num_rays)
+
+true_obj = generate_Shepp_Logan(original_data_shape)
+
+save_cube(true_obj, base_folder + "true")
+
+theta = np.arange(0, 180., 180. / num_angles)*np.pi/180.
+
+simulation = forward_project(true_obj, theta)
+
+save_cube(true_obj, base_folder + "sim")
+
+sinogram = qt[0]
+
+print("\n\n ---------- Input ---------- \n\n")
+print(sinogram)
+
+qxy = grid_rec_one_slice(sinogram, num_rays)
+
+
+print("\n\n ---------- Finished one pass of grid rec ---------- \n\n")
 result = np.fft.fftshift(np.fft.ifft2(qxy), axes=1)
-print(result.shape)
-#print(result)
+
+print("\n\n ---------- Output ---------- \n\n")
+
+print(result)
+
+
+
+
