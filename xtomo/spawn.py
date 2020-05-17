a42df663b578872d010d44da6cee5f5c4c54d95c
smarchesini@sigray.com <smarchesini@sigray.com>
2020-05-17 09:05:50 -0700

    spawn with memory-mapping and transpose

--- xtomo/spawn.py
+++ xtomo/spawn.py
@@ -103,4 +103,90 @@ def reconstruct_mpiv(sino, theta, rot_center, n_workers, Dopts):
     comm.Disconnect()
     return shared_tomo
     #comm.Free()
+    
+
+import tempfile
+tempdir = tempfile.gettempdir()+'/xpack/'
+
+import os.path as path
+import os
+try:
+    os.mkdir(tempdir)
+except: 
+    None
+        
+
+import numpy as np
+
+def mmdata(shp, fname, mode='w+'):
+    filename = path.join(tempdir, fname)
+    
+    fp = np.memmap(filename, dtype='float32', mode=mode, shape=shp)
+    return fp
+    #shared_sino = xtomo.communicator.allocate_shared(sino_shape, comm = comm_intra)
+
+def reconstruct_mpimm(sino, theta, rot_center, n_workers, Dopts, order='sino'):
+
+    import xtomo
+    import xtomo.communicator
+    import numpy as np
+    
+    if order == 'proj':
+        sino=np.swapaxes(sino,0,1)
+    
+    arg1=xtomo.__path__.__dict__["_path"][0]+'/mpi_worker_mm.py'
+    
+    comm = MPI.COMM_WORLD.Spawn(
+        executable,
+        args = [arg1,], #args=[sys.argv[0], start_worker],
+        maxprocs=n_workers)
+    
+    comm_intra = comm.Merge(MPI.COMM_WORLD)    
+    
+    print('spawned size',comm_intra.Get_size())
+
+
+    # broadcast the data   
+    sino_shape = sino.shape
+    theta_shape = theta.shape
+    num_slices =sino_shape[0]
+    num_rays = sino_shape[2]
+    tomo_shape = (num_slices, num_rays, num_rays)
+
+    comm_intra.bcast(sino_shape,root=0)    
+    comm_intra.bcast(theta_shape,root=0)    
+    comm_intra.bcast(rot_center,root=0)    
+    comm_intra.bcast(Dopts,root=0)
+    
+    #print('broadcasted')
+
+    # sinogram in shared memory
+
+    shared_sino = mmdata(sino_shape, 'data', mode='w+')
+    #print('sinogram on file')
+
+    shared_theta = mmdata(theta_shape, 'theta', mode = 'w+')
+    shared_tomo =mmdata(tomo_shape, 'tomo', mode='w+')
+    
+    shared_sino[...]=sino
+    shared_theta[...]=theta  
+
+    #print('tomo on file')
+
+    # make sure the files exist
+    comm_intra.barrier()
+
+    # doing the reconstruction
+    comm_intra.barrier()
+
+    
+    comm_intra.barrier()
+
+    
+
+    comm_intra.Free()
+    del comm_intra    
+    comm.Disconnect()
+    return shared_tomo
+    #comm.Free()
 
